bedeb054f12e3305ddef89d0d4172bc396159f4a
Hasby <mhdhsbyy@gmail.com>
2025-05-13 15:47:27 +0700

    menambahkan tombol untuk scrol keatas halaman

--- js/script.js
+++ js/script.js
@@ -1,17 +1,41 @@
-// Toggle class active 
+// Toggle class active
 const navbarNav = document.querySelector(".navbar-nav");
 
 // ketika menu di klick
 document.querySelector("#hamburger-menu").onclick = () => {
-    navbarNav.classList.toggle("active");
+  navbarNav.classList.toggle("active");
 };
 
 // klick diluar sidebar untuk menghilangkan nav
 
 const hamburger = document.querySelector("#hamburger-menu");
 
-document.addEventListener("click", function(e) {
-    if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)){
-        navbarNav.classList.remove("active");
-    }
+document.addEventListener("click", function (e) {
+  if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove("active");
+  }
+});
+
+// Ambil elemen tombol
+const scrollToTopBtn = document.getElementById("scrollToTopBtn");
+
+// Event listener untuk scroll
+window.onscroll = function () {
+  if (
+    document.body.scrollTop > 500 ||
+    document.documentElement.scrollTop > 500
+  ) {
+    scrollToTopBtn.style.display = "block"; // Menampilkan tombol jika scroll > 500px
+  } else {
+    scrollToTopBtn.style.display = "none"; // Sembunyikan tombol jika scroll < 500px
+  }
+};
+
+// Event listener untuk klik tombol
+scrollToTopBtn.addEventListener("click", function () {
+  // Kembali ke atas dengan smooth scrolling
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
 });
